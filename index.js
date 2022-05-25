fcd1a696c132bb6a6eb0b19134f40c42847717e0
Sidhan Shamil <89472677+sidhan-shamil@users.noreply.github.com>
2022-05-25 18:52:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 const Joi = require('joi');
-
 const express = require('express');
 const app = express();
 
+//For json format converting
 app.use(express.json())
 
-
+//Sample data
 const courses = [ 
     {id: 1, name: 'course one'},
     {id: 2, name: 'course two'},
     {id: 3, name: 'course three'}
 ];
 
-
+//Get root page
 app.get('/', (req, res) => {
     console.log(courses);
     res.send('Hello, hi')
 })
 
+//Get admin page
 app.get('/admin', (req, res) => {
     res.send('Admin page...');
 })
 
+//Get all courses
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
-//id check
+//ID check
 app.get('/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt (req.params.id));
     if(!course) res.status(404).send('Not found ( Error404 )')
     res.send(course);
 })
 
-//add/ create
+//Add/ Create
 app.post('/courses', (req, res) => {
 
     const {error} = validateCourse(req.body)  
@@ -66,31 +68,16 @@ function validateCourse(course){
     return Joi.validate(course);
 }
 
+//Delete
 app.delete('/courses/:id', (req, res)=> {
     const course = courses.find(c => c.id === parseInt (req.params.id));
     if(!course) return res.status(404).send('Not found ( Error404 )')
-
     const index = courses.indexOf(course);
     courses.splice(index, 1)
-    
     res.send(course);
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}...`));
 
